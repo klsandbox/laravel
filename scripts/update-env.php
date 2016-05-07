c413af5ab510dd904fc71f99535f674eec009d68
Klsandbox <ibrahim@klsandbox.com>
2016-05-07 10:48:51 +0800

    simpler-env

--- scripts/update-env.php
+++ scripts/update-env.php
@@ -1,15 +1,10 @@
 <?php
-$name = substr(basename(dirname(dirname(__FILE__))), 0, 16);
-echo $name . PHP_EOL;
-
 $envFilename = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '.env.example';
 $envOutFilename = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '.env';
 $envFile = file_get_contents($envFilename);
 
 $string = bin2hex(openssl_random_pseudo_bytes(10));
 
-$envFile = preg_replace('/DB_DATABASE=\w+/', 'DB_DATABASE=' . $name, $envFile);
-$envFile = preg_replace('/DB_USERNAME=\w+/', 'DB_USERNAME=' . $name, $envFile);
 $envFile = preg_replace('/DB_PASSWORD=\w+/', 'DB_PASSWORD=' . $string, $envFile);
 
 file_put_contents($envOutFilename, $envFile);
